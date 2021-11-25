2c4db232cbe80643e24b38e61fa1ea207a487571
lot8229 <lot8229@kakao.com>
2021-11-25 20:36:47 +0900

    [EDIT] 파이썬 코드 수정

--- src/python/ODyolo.py
+++ src/python/ODyolo.py
@@ -1,13 +1,17 @@
 import torch
 import sys
+import json
+
 torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
 def ObjectDetection(img):
     model = torch.load('./yolo.pth')
     result = model(img)
     df = result.pandas().xyxy[0]
-
-    print(list(df['name']))
-
-if __name__ == '__main__':
-    ObjectDetection(sys.argv[1])
+    json_return = {
+        "class" : list(df["name"]) 
+    }
+    json_string = json.dumps(json_return)
+    return json_string
+# if __name__ == '__main__':
+#     ObjectDetection(sys.argv[1])
